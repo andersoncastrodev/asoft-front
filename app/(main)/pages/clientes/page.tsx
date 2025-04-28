06648731893b7e94ae544dfa9fd6189c455d73c4
Anderson Castro <andersoncastrosantos@gmail.com>
2025-04-28 08:50:55 -0300

    .

--- app/(main)/pages/clientes/page.tsx
+++ app/(main)/pages/clientes/page.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Clientes = () => {
     return (
         <div className="grid">
-            <div className="col-12">
+            {/* <div className="col-12">
                 <div className="card">
                     {/* <h5>Pagina de Clientes</h5>
                     <p>Aqui vai a lista de clientes</p> */}
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 };
